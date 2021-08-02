4132a42770e412d9e2a4acfc0ab1ddae952de3bd
fulbertgil <30720813+fulbertgil@users.noreply.github.com>
2021-08-02 11:24:42 +0800

    test
    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-//const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -32,7 +32,7 @@ app.use(logger(config.isProd ? 'combined' : 'dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(favicon(path.join(__dirname, 'public', 'favicon/favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon/favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // bootstrap routes
